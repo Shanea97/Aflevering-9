9ec3fa7b79fda32d0dbce9f4a6d35249e3d7697e
Bertram P <bertramherlov@gmail.com>
2018-12-05 13:43:45 +0100

    9i1 fungere

--- 9i1.fsx
+++ 9i1.fsx
@@ -1,4 +1,6 @@
 open System.Text.RegularExpressions
+open System
+open System.IO
 /// dette er pt taget fra bogen af Jon Sporring 
 let url2stream url = 
     let uri = System.Uri url 
@@ -19,26 +21,26 @@ let html = readURL url
 printfn "Downloaded: %A. First %A Charecters are:\n %A" url a html.[0..a]
 // printfn "Dette er HTML funktionen:%A" html
 
-let c = Regex "<a href"
+// let c = Regex "<a href"
 
-let f = c.IsMatch (html)
+// let f = c.IsMatch (html)
 
-let e = c.IsMatch "<a href=\"#\">Link tekst</a>"
+// let e = c.IsMatch "<a href=\"#\">Link tekst</a>"
 
-printfn "Is.match på c: %A" e
+// printfn "Is.match på c: %A" e
 
-printfn "IsMatch på f: %A" f
+// printfn "IsMatch på f: %A" f
 
-let str = "<a href"
-let strLength = String.length str
-let mutable count = 0 
-for i = 0 to strLength-1 do 
-    let strOut = str.[i..(strLength-1)]
-    let strOutLength = String.length strOut
-    let strText = str.[i..strLength-1]
-    if strText = "href" || strText = "ref" then 
-        count <- count+1 
-    printfn "String output:%A\nString length: %A\n Counter: %A\n String text: %A" strOut strOutLength count strText
+// let str = "<a href"
+// let strLength = String.length str
+// let mutable count = 0 
+// for i = 0 to strLength-1 do 
+//     let strOut = str.[i..(strLength-1)]
+//     let strOutLength = String.length strOut
+//     let strText = str.[i..strLength-1]
+//     if strText = "href" || strText = "ref" then 
+//         count <- count+1 
+//     printfn "String output:%A\nString length: %A\n Counter: %A\n String text: %A" strOut strOutLength count strText
 
 
 /// Brug String.split til at klare næste opgave. 
@@ -52,13 +54,12 @@ let CountLinks (url:string) : int =
     let HtmlLength = String.length (HTML)
     let mutable count = 0
     for i = 0 to HtmlLength-1-strLength do
-        let strText = HTML.[i..(i+strLength)]
+        let strText = HTML.[i..(i+strLength-1)]
         if strText = str then
             count <- count + 1
     count
-printfn "%A"  (CountLinks url)
+printfn "How many matches do we get on (<a href): %A"  (CountLinks url)
 
 
-// let koo = html
-// let moo = String.length koo 
-// printfn "%A" moo
+
+/// Kan umiddelbart ikke finde svare på hvorfor at denne ikke burde virke ?
