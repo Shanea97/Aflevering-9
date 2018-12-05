de34478ca2f89fbb0d25fce7dec55e919a96931c
Bertram P <bertramherlov@gmail.com>
2018-12-05 13:03:48 +0100

    mere opdatering

--- 9i2.fsx
+++ 9i2.fsx
@@ -1,6 +1,7 @@
 open System
 open System.IO
 
+
 let Lines = File.ReadAllLines (@"/Users/bertrampedersen/Documents/GitHub/Aflevering-9/storeClausLilleClaus.txt")
 
 let list = Seq.toList Lines 
@@ -11,10 +12,6 @@ let strings = ["one";"two";"three"]
 let read = str |> List.fold (+) "  "
 let sndread = String.concat " " str
 let LilleOgStoreKlaus = String.concat " " Lines
-// printfn "List.fold: %s" read 
-// printfn "%A" str
-// printfn "Second Read: %s" sndread 
-// printfn "Lille og Store Klaus :\n %s " LilleOgStoreKlaus
 let ListSplit = LilleOgStoreKlaus.Split [|' '|]
 
 let words = ListSplit |> Array.filter (fun x -> x = "Claus")
@@ -24,8 +21,13 @@ let længde = Array.length ListSplit
 for i = 0 to længde-1 do 
     let word = ListSplit.[i]
     let list = ListSplit |> Array.filter (fun x -> x = word)
-    printfn "%A" list 
+    let arraylength = list.Length
 
-// let xmd = [|"23";"42";"54";"14";"54";"66";"22"|]
 
-// printfn "Array længde: %A" længde 
+    let tupple = (list.[0], arraylength)
+    // let NewArray = Array.map2 (fun x y -> (x,y) )list NewNewArray
+    // printfn "%A" list 
+    // printfn "længden af arrayet; %A"  arraylength
+    printfn "En tupple som viser ordet, og antal gentagelser: %A" tupple
+    // printfn "NewArray: %A" NewArray
+
--- test.fsx
+++ test.fsx
@@ -1,10 +1,10 @@
-let safeIndexIf (arr: 'a array) (i:int) : 'a =
-    if i > arr.Length then 
-       arr.[i]
-    else 
-        arr.[i]
-printfn "%A" (safeIndexIf [|1;2;3;4;5;6;7;8;9;10|] 2)
-printfn "%A" (safeIndexIf [|1;2;3;4;5;6;7;8;9;10|] 50)
+// let safeIndexIf (arr: 'a array) (i:int) : 'a =
+//     if i > arr.Length then 
+//        arr.[i]
+//     else 
+//         arr.[i]
+// printfn "%A" (safeIndexIf [|1;2;3;4;5;6;7;8;9;10|] 2)
+// printfn "%A" (safeIndexIf [|1;2;3;4;5;6;7;8;9;10|] 50)
 
 // let safeIndexTry (arr: 'a array) (i:int) : 'a = 
 //     try 
@@ -17,3 +17,11 @@ printfn "%A" (safeIndexIf [|1;2;3;4;5;6;7;8;9;10|] 50)
 // printfn "%A" abc
 // printfn "%A\n" (safeIndexTry [|"1";"2";"3";"4";"5";"6";"7"|] 50 ) 
 // /// Jeg har ingen ide om hvordan jeg skal få det til at virke med alle typer .... 
+
+open System
+open System.IO
+
+let filename = File.Create "Filnavn123.txt"
+let text = StreamWriter "Hej med dig"
+text
+filename
